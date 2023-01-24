d74ab120bf5fa7fb1f33b3c2d84021cf2245e63b
OmarJ9 <businessomar79@gmail.com>
2023-01-24 16:20:52 +0100

    ADD Profile Screen

--- lib/src/presentation/screens/profile_screen/profile_screen.dart
+++ lib/src/presentation/screens/profile_screen/profile_screen.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:movie_finder/src/blocs/Auth/authentication_bloc.dart';
+import 'package:movie_finder/src/constants/assets_constants.dart';
+import 'package:movie_finder/src/presentation/widgets/custom_text.dart';
 import 'package:movie_finder/src/utils/extensions/extensions.dart';
+import 'package:sizer/sizer.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../config/app_route.dart';
 import '../../widgets/custom_button.dart';
@@ -12,6 +16,7 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    User user = Supabase.instance.client.auth.currentUser!;
     return BlocListener<AuthenticationBloc, AuthenticationState>(
       listener: (context, state) {
         if (state is UnAuthenticatedState) {
@@ -20,12 +25,47 @@ class ProfileScreen extends StatelessWidget {
           context.showsnackbar(title: 'Something Went Wrong!');
         }
       },
-      child: Center(
-        child: CustomButton(
-            func: () {
-              context.read<AuthenticationBloc>().add(const SignOutEvent());
-            },
-            title: 'Sign Out'),
+      child: SizedBox(
+        width: 100.w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              AssetsConstants.placeholder,
+              height: 20.h,
+            ),
+            SizedBox(
+              height: 1.h,
+            ),
+            CustomText(
+              title:
+                  "Welcome\n ${(user.email == "omar@gmail.com") ? "Anonymous" : user.email}",
+              maxlines: 3,
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            CustomButton(
+                func: () {
+                  context.read<AuthenticationBloc>().add(const SignOutEvent());
+                },
+                title: 'Sign Out'),
+            SizedBox(
+              height: 2.h,
+            ),
+            (user.email == "omar@gmail.com")
+                ? const SizedBox()
+                : CustomButton(
+                    color: Colors.red,
+                    func: () {
+                      context
+                          .read<AuthenticationBloc>()
+                          .add(const SignOutEvent());
+                    },
+                    title: 'Remove all saved data'),
+          ],
+        ),
       ),
     );
   }
